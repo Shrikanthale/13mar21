0198c0d4484e59e2c4b1d8406663744fbd5222c7
Shrikanthale <79976234+Shrikanthale@users.noreply.github.com>
2021-03-19 05:38:13 -0700

    Add files via upload

---
+++ 13mar21/datatype.js
@@ -0,0 +1,37 @@
+//var isvalid="shri"
+//console.log(isvalid)
+//object 
+// var varuablename = {key : value , key : value}
+//var student={Name:"shri",Address:"mgm",Rollno:144,isGraduated:false}; 
+//console.log(student)
+//console.log(student.Address) 
+// null is used in terms with objects....
+//var name1= null;
+//var first = 10+30+"shri"
+//console.log(first) 
+//var second = "shri"+10+30 
+//console.log(second)  
+
+//condition 
+/*var number = 10;
+//console.log(number)
+ //console.log(number+"is odd number") 
+
+if(number%2==="0"){
+    console.log(number+"is Even number")
+}
+*/
+
+//if(number%2!=0){
+  //  console.log(number+"is odd number")
+//}
+
+/*else{
+    console.log(number+"is odd number")
+}
+*/
+count = 10 
+while(count>0){
+    console.log("Shrikant")
+    count=count - 1
+} 
---
+++ 13mar21/loop.js
@@ -0,0 +1,38 @@
+/*count = 3 
+while(count>0){
+    console.log("Shrikant")
+    count=count - 1
+    console.log("value of count is "+count)
+    if(count==0){
+        console.log("oops overload")
+    }
+} 
+*/
+/*
+for(var i=0; i<4; i++){
+    console.log(" hello x " +i);
+}
+console.log("done");
+*/
+/*
+month = 3
+switch(month){
+    case 1 :
+        console.log("winter");
+        break;
+        
+    case 2 :
+        console.log("summer");
+        break;
+        
+    case 3 :
+        console.log("rainy");
+        break;
+        
+        default:
+            console.log("error");
+}
+*/
+var marks = 40
+var result = marks<35? "fail":"pass"
+console.log(result)
